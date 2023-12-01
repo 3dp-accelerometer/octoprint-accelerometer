61df7d6eee3cc4cd03adcdf962b93479f57cd523
Raoul Rubien <rubienr@sbox.tugraz.at>
2023-12-01 19:35:30 +0100

    implements d3js data visualization proof of concept

--- octoprint_accelerometer/static/js/datavis.js
+++ octoprint_accelerometer/static/js/datavis.js
@@ -1,79 +1,100 @@
-chart_data = async function() {
-    return d3.dsv(" ",
-           "plugin/octoprint_accelerometer/download/axxel-30f9c95c-20231127-235625233-s000-ax-f010-z015.tsv",
-           (d) => {
-               return {
-                   seq:   parseFloat(d.seq),
-                   sample: parseFloat(d.sample),
-                   x:     parseFloat(d.x),
-                   y:     parseFloat(d.y),
-                   z:     parseFloat(d.z),
-               };
-           });
-};
+"use strict";
 
-chart = async function(data) {
-      // Declare the chart dimensions and margins.
-  const width = 640;
-  const height = 400;
-  const marginTop = 20;
-  const marginRight = 20;
-  const marginBottom = 30;
-  const marginLeft = 40;
+class OctoAxxelDataVis {
 
-  // Declare the x (horizontal position) scale.
-  const x = d3.scaleLinear()
-      .domain([0, 5000])
-      .range([marginLeft, width - marginRight]);
+    /**
+     * @param {str} fileUrl - URL of tabular separated file, i.e. "plugin/octoprint_accelerometer/download/axxel-30f9c95c-20231127-235625233-s000-ax-f010-z015.tsv"
+     * @param {char} separator - tabular separator (single character)
+     * @return {[{seq: float, sample: float, x: float, y: float, z: float}]}
+     */
+    async fetchAccelerationData(fileUrl, separator = " ") {
+        return d3.dsv(separator, fileUrl, (line) => {
+                return {
+                    seq:    parseFloat(line.seq),
+                    sample: parseFloat(line.sample),
+                    x:      parseFloat(line.x),
+                    y:      parseFloat(line.y),
+                    z:      parseFloat(line.z),
+                }
+            }
+        );
+    }
 
-  // Declare the y (vertical position) scale.
-  const y = d3.scaleLinear()
-      .domain([-10000, 10000])
-      .range([height - marginBottom, marginTop]);
+    /**
+     * @param {[{seq: float, sample: float, x: float, y: float, z: float}]}: data - chart data to plot
+     */
+    async computeAccelerationChart(data) {
+        // declare the chart dimensions and margins
+        const width = 640;
+        const height = 400;
+        const marginTop = 20;
+        const marginRight = 20;
+        const marginBottom = 30;
+        const marginLeft = 40;
 
-  // Create the SVG container.
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [0, 0, width, height])
-      .attr("style", "max-width: 100%; height: auto; overflow: visible; font: 10px sans-serif;");
+        // declare the x (horizontal position) scale
+        const x = d3.scaleLinear()
+            .domain([0, 5000])
+            .range([marginLeft, width - marginRight]);
 
-  // Add the horizontal axis.
-  svg.append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+        // declare the y (vertical position) scale
+        const y = d3.scaleLinear()
+            .domain([-10000, 10000])
+            .range([height - marginBottom, marginTop]);
 
-  // Add the vertical axis.
-  svg.append("g")
-      .attr("transform", `translate(${marginLeft},0)`)
-      .call(d3.axisLeft(y).ticks(height / 40))
-      .call(g => g.select(".domain").remove())
-      .call(g => g.selectAll(".tick line").clone()
-          .attr("x2", width - marginLeft - marginRight)
-          .attr("stroke-opacity", 0.1))
-      .call(g => g.append("text")
-          .attr("x", -marginLeft)
-          .attr("y", 10)
-          .attr("fill", "currentColor")
-          .attr("text-anchor", "start")
-          .text("↑ Daily close ($)"));
+        // create the SVG container
+        const svg = d3.create("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [0, 0, width, height])
+            .attr("style", "max-width: 100%; height: auto; overflow: visible; font: 10px sans-serif;");
+
+        // add the horizontal axis
+        svg.append("g")
+            .attr("transform", `translate(0,${height - marginBottom})`)
+            .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+            .call(g => g.append("text")
+                .attr("x", height - marginBottom)
+                .attr("y", width - 10)
+                .attr("fill", "currentColor")
+                .attr("text-anchor", "start")
+                .text("time [s]"));
+
+        // add the vertical axis
+        svg.append("g")
+            .attr("transform", `translate(${marginLeft},0)`)
+            .call(d3.axisLeft(y).ticks(height / 40))
+            .call(g => g.select(".domain").remove())
+            .call(g => g.selectAll(".tick line").clone()
+                .attr("x2", width - marginLeft - marginRight)
+                .attr("stroke-opacity", 0.1))
+            .call(g => g.append("text")
+                .attr("x", -marginLeft)
+                .attr("y", 10)
+                .attr("fill", "currentColor")
+                .attr("text-anchor", "start")
+                .text("↑ acc [mg]"));
 
 
-    data.forEach(function(d) {
-        console.log(d)
-    });
+        // data.forEach((d) => console.log(d));
 
-     // Draw the lines.
-  const line = d3.line()
-        .x(d => x(d.sample))
-        .y(d => y(d.z));
+        // draw the lines
+        const line = d3.line()
+            .x(d => x(d.sample))
+            .y(d => y(d.z));
 
-  svg.append("path")
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", line(data));
+        svg.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", line(data));
 
-  return self.svg = svg.node();
+        return self.svg = svg.node();
+    }
 
-};
+    async plot(fileUrl, divNodeId) {
+        const data = await this.fetchAccelerationData(fileUrl);
+        const chart = await this.computeAccelerationChart(data);
+        document.querySelector(divNodeId).append(chart);
+    }
+}
