f10ee72ccdb1e8a45e75514c5a6978293306b715
Raoul Rubien <rubienr@sbox.tugraz.at>
2023-12-02 23:56:40 +0100

    refines acceleration vis.: tool-tip and path highlight on mouse over path; auto x/y-axis extent

--- octoprint_accelerometer/static/js/datavis.js
+++ octoprint_accelerometer/static/js/datavis.js
@@ -7,6 +7,7 @@ const DIV_ID_ACCELERATION_VIS = "tab_plugin_octoprint_acceleration_vis";
 const DIV_ID_FFT_VIS = "tab_plugin_octoprint_fft_vis";
 
 class OctoAxxelDataSetVis {
+    // blueprint: https://observablehq.com/@d3/indented-tree?intent=fork
 
     /**
      * @param {str} dataSetUrl - URL for GET request, i.e. "plugin/octoprint_accelerometer/get_data_listing"
@@ -46,7 +47,7 @@ class OctoAxxelDataSetVis {
     }
 
     async computeChart(data) {
-        const format = d3.format(",");
+        const format = d3.format("");
         const nodeSize = 17;
         const root = d3.hierarchy(data).eachBefore((i => d => d.index = i++)(0));
         const nodes = root.descendants();
@@ -105,15 +106,22 @@ class OctoAxxelDataSetVis {
             .attr("filename", d => {
                 if ("stream" in d.data.data) return d.data.data.stream.filename_no_ext + "."+ d.data.data.stream.file_extension;
                 if ("fft" in d.data.data) return d.data.data.fft.filename_no_ext + "."+ d.data.data.fft.file_extension;
-                return undefined;
-                })
+                return undefined;})
+            .attr("nodeType", d => {
+                if ("stream" in d.data.data) return "stream";
+                if ("fft" in d.data.data) return "fft";
+                return undefined;})
             .text(d => d.data.name)
             .on("pointerenter", event => event.target.setAttribute("style", "font-weight:bold;cursor: pointer;"))
             .on("pointerleave", event => event.target.setAttribute("style", "font-weight:normal;cursor: pointer;"))
             .on("click", event => {
                 const fileName = event.target.getAttribute("filename");
-                console.log(fileName);
-                (async () => new OctoAxxelAccelerationVis().plot(fileName))();
+                const nodeType = event.target.getAttribute("nodeType");
+                if (nodeType === "stream") {
+                    console.log("plotting acceleration: " + fileName);
+                    (async () => new OctoAxxelAccelerationVis().plot(fileName))();
+                }
+                else if (nodeType === "fft") { console.log("TODO: plotting FFT"); }
             });
 
         node.append("title")
@@ -164,6 +172,10 @@ class OctoAxxelDataSetVis {
 }
 
 class OctoAxxelAccelerationVis {
+    // blueprints:
+    //  - mouse events: https://observablehq.com/@d3/multi-line-chart/2?intent=fork
+    //  - simple line chart: https://observablehq.com/@d3/line-chart/2?intent=fork
+    //  - zoom: https://observablehq.com/@d3/zoomable-bar-chart?intent=fork
 
     /**
      * @param {str} fileUrl - URL of tabular separated file, i.e. "plugin/octoprint_accelerometer/download/axxel-30f9c95c-20231127-235625233-s000-ax-f010-z015.tsv"
@@ -171,23 +183,46 @@ class OctoAxxelAccelerationVis {
      * @return {[{seq: float, sample: float, x: float, y: float, z: float}]}
      */
     async fetchData(fileUrl, separator = " ") {
-        return d3.dsv(separator, fileUrl, (line) => {
-                return {
-                    seq:    parseFloat(line.seq),
-                    sample: parseFloat(line.sample),
-                    x:      parseFloat(line.x),
-                    y:      parseFloat(line.y),
-                    z:      parseFloat(line.z),
+        const text = await d3.text(fileUrl);
+
+        // parse meta data which is the very last line of a complete stream
+        let meta = undefined;
+        const lines = text.split("\n");
+        for (let idx = lines.length - 1; idx > lines.length - 3; idx--) {
+            const line = lines[idx];
+            if (line.startsWith("#")) {
+                try {
+                    let metaStr = line.replace(/(^.*#\s*)/, "");
+                    meta = JSON.parse(metaStr);
+                    const odrToFloat = (odr) => parseFloat(odr.replace(/(^ODR)/, ""));
+                    meta.rate = odrToFloat(meta.rate);
+                    break;
+                } catch {
+                    console.error("failed to parse metadata from " + fileUrl);
                 }
             }
+        }
+
+        const dsv = d3.dsvFormat(" ").parse(text, (line) => {
+                return {
+                    seq: parseFloat(line.seq),
+                    sample: parseFloat(line.sample),
+                    timestamp_ms: parseFloat(line.sample) * 1000 / meta.rate,
+                    x: parseFloat(line.x),
+                    y: parseFloat(line.y),
+                    z: parseFloat(line.z),
+                };
+            }
         );
+
+        return dsv;
     }
 
     /**
      * @param {[{seq: float, sample: float, x: float, y: float, z: float}]}: data - chart data to plot
      */
     async computeChart(data) {
-        // declare the chart dimensions and margins
+        const format = d3.format("+r");
         const width = 640;
         const height = 400;
         const marginTop = 20;
@@ -195,15 +230,17 @@ class OctoAxxelAccelerationVis {
         const marginBottom = 30;
         const marginLeft = 40;
 
-        // declare the x (horizontal position) scale
-        const x = d3.scaleLinear()
-            .domain([0, 5000])
+        // declare the x scale (time domain)
+        const xScale = d3.scaleLinear()
+            .domain(d3.extent(data, d => d.timestamp_ms)).nice()
             .range([marginLeft, width - marginRight]);
+        const xAxis = d3.axisBottom(xScale).ticks(width / 80, format).tickSizeOuter(0);
 
-        // declare the y (vertical position) scale
-        const y = d3.scaleLinear()
-            .domain([-10000, 10000])
+        // declare the y scale (acceleration domain)
+        const yScale = d3.scaleLinear()
+            .domain([d3.min(data, d => Math.min(d.x, d.y, d.z)), d3.max(data, d => Math.max(d.x, d.y, d.z))]).nice()
             .range([height - marginBottom, marginTop]);
+        const yAxis = d3.axisLeft(yScale).ticks(height / 40, format)
 
         // create the SVG container
         const svg = d3.create("svg")
@@ -212,21 +249,21 @@ class OctoAxxelAccelerationVis {
             .attr("viewBox", [0, 0, width, height])
             .attr("style", "max-width: 100%; height: auto; overflow: visible; font: 10px sans-serif;");
 
-        // add the horizontal axis
+        // time axis
         svg.append("g")
             .attr("transform", `translate(0,${height - marginBottom})`)
-            .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+            .call(xAxis)
             .call(g => g.append("text")
-                .attr("x", height - marginBottom)
-                .attr("y", width - 10)
+                .attr("x", width / 2)
+                .attr("y", marginBottom )
                 .attr("fill", "currentColor")
                 .attr("text-anchor", "start")
-                .text("time [s]"));
+                .text("time [ms] →"));
 
-        // add the vertical axis
+        // acceleration axis
         svg.append("g")
             .attr("transform", `translate(${marginLeft},0)`)
-            .call(d3.axisLeft(y).ticks(height / 40))
+            .call(yAxis)
             .call(g => g.select(".domain").remove())
             .call(g => g.selectAll(".tick line").clone()
                 .attr("x2", width - marginLeft - marginRight)
@@ -239,15 +276,86 @@ class OctoAxxelAccelerationVis {
                 .text("↑ acc [mg]"));
 
         // draw the lines
-        const line = d3.line()
-            .x(d => x(d.sample))
-            .y(d => y(d.z));
+        const xAccelerationPath = d3.line(d => xScale(d.timestamp_ms), d => yScale(d.x));
+        const yAccelerationPath = d3.line(d => xScale(d.timestamp_ms), d => yScale(d.y));
+        const zAccelerationPath = d3.line(d => xScale(d.timestamp_ms), d => yScale(d.z));
 
-        svg.append("path")
+        const pathAccX = svg.append("path")
             .attr("fill", "none")
-            .attr("stroke", "steelblue")
+            .attr("stroke", "Tomato")
             .attr("stroke-width", 1.5)
-            .attr("d", line(data));
+            .attr("d", xAccelerationPath(data));
+
+        const pathAccY = svg.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "MediumSeaGreen")
+            .attr("stroke-width", 1.5)
+            .attr("d", yAccelerationPath(data));
+
+        const pathAccZ = svg.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "SteelBlue")
+            .attr("stroke-width", 1.5)
+            .attr("d", zAccelerationPath(data));
+
+        // invisible layer for the interactive tip
+        const dot = svg.append("g")
+            .attr("display", "none");
+        dot.append("circle")
+            .attr("r", 2.5);
+        dot.append("text")
+            .attr("id", "text_l1")
+            .attr("text-anchor", "left")
+            .attr("y", -24);
+        dot.append("text")
+            .attr("id", "text_l2")
+            .attr("text-anchor", "left")
+            .attr("y", -16);
+        dot.append("text")
+            .attr("id", "text_l3")
+            .attr("text-anchor", "left")
+            .attr("y", -8);
+
+        const pointerentered = () => {
+            pathAccX.style("mix-blend-mode", null).style("stroke", "#ddd");
+            pathAccY.style("mix-blend-mode", null).style("stroke", "#ddd");
+            pathAccZ.style("mix-blend-mode", null).style("stroke", "#ddd");
+            dot.attr("display", null);
+        }
+
+        const pointerleft = () => {
+            pathAccX.style("mix-blend-mode", "multiply").style("stroke", null);
+            pathAccY.style("mix-blend-mode", "multiply").style("stroke", null);
+            pathAccZ.style("mix-blend-mode", "multiply").style("stroke", null);
+            dot.attr("display", "none");
+            svg.node().value = null;
+            svg.dispatch("input", {bubbles: true});
+        }
+
+        const points = data.map((d) => [xScale(d.timestamp_ms), yScale(d.x), "x"])
+            .concat(data.map((d) => [xScale(d.timestamp_ms), yScale(d.y), "y"]))
+            .concat(data.map((d) => [xScale(d.timestamp_ms), yScale(d.z), "z"]));
+
+        const pointermoved = (event) => {
+            const [xm, ym] = d3.pointer(event);
+            const i = d3.leastIndex(points, ([x, y]) => Math.hypot(x - xm, y - ym));
+            const [x, y, axis] = points[i];
+
+            pathAccX.style("stroke", "x" === axis ? null : "#ddd");
+            pathAccY.style("stroke", "y" === axis ? null : "#ddd");
+            pathAccZ.style("stroke", "z" === axis ? null : "#ddd");
+
+            dot.attr("transform", `translate(${x},${y})`);
+            dot.select("#text_l1").text("axis: " + axis.toUpperCase());
+            dot.select("#text_l2").text("time: " + Math.round(xScale.invert(x)) + "ms");
+            dot.select("#text_l3").text("acc: " + Math.round(yScale.invert(y)) + "mg");
+            svg.property("value", points[i]).dispatch("input", {bubbles: true});
+        }
+
+        svg
+            .on("pointerenter", pointerentered)
+            .on("pointerleave", pointerleft)
+            .on("pointermove", pointermoved);
 
         return svg.node();
     }
