55dfc7d267f9673636eaea9de588766f453699c5
Raoul Rubien <rubienr@sbox.tugraz.at>
2023-12-02 09:20:19 +0100

    data set visualization proof of concept

--- octoprint_accelerometer/static/js/datavis.js
+++ octoprint_accelerometer/static/js/datavis.js
@@ -1,13 +1,131 @@
 "use strict";
 
-class OctoAxxelDataVis {
+class OctoAxxelDataSetVis {
+
+    /**
+     * @param {str} dataSetUrl - URL for GET request, i.e. "plugin/octoprint_accelerometer/get_data_listing"
+     * @return {object}
+     */
+    async fetchData(dataSetUrl) {
+        const response = await fetch(dataSetUrl);
+        const rawData = await response.json();
+        const rootNode = rawData["data_sets"]
+
+        const runs = [];
+        for (const runId in rootNode) {
+            const runNode = rootNode[runId];
+            const streams = [];
+            for (const streamId in runNode) {
+                const streamNode = runNode[streamId];
+                const ffts = [];
+                for (const fftsId in streamNode["fft"]) {
+                    const fileName = streamNode["fft"][fftsId]["filename_no_ext"];
+                    ffts.push({name: fileName});
+                }
+                streams.push({name: streamId, children: ffts});
+            }
+            runs.push({name: runId, children: streams});
+        }
+        const data = {name: "root", children: runs}
+        return data;
+    }
+
+    async computeChart(data) {
+        const format = d3.format(",");
+        const nodeSize = 17;
+        const root = d3.hierarchy(data).eachBefore((i => d => d.index = i++)(0));
+        const nodes = root.descendants();
+        const width = 928;
+        const height = (nodes.length + 1) * nodeSize;
+
+        const columns = [
+            {
+                label: "Size",
+                value: d => d.value,
+                format,
+                x: 480
+            },
+            {
+                label: "Count",
+                value: d => d.children ? 0 : 1,
+                format: (value, d) => d.children ? format(value) : "-",
+                x: 640
+            }
+        ];
+
+        const svg = d3.create("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [-nodeSize / 2, -nodeSize * 3 / 2, width, height])
+            .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif; overflow: visible;");
+
+        const link = svg.append("g")
+            .attr("fill", "none")
+            .attr("stroke", "#999")
+        .selectAll()
+        .data(root.links())
+        .join("path")
+            .attr("d", d => `
+                M${d.source.depth * nodeSize},${d.source.index * nodeSize}
+                V${d.target.index * nodeSize}
+                h${nodeSize}
+          `);
+
+        const node = svg.append("g")
+            .selectAll()
+            .data(nodes)
+            .join("g")
+                .attr("transform", d => `translate(0,${d.index * nodeSize})`);
+
+        node.append("circle")
+            .attr("cx", d => d.depth * nodeSize)
+            .attr("r", 2.5)
+            .attr("fill", d => d.children ? null : "#999");
+
+        node.append("text")
+            .attr("dy", "0.32em")
+            .attr("x", d => d.depth * nodeSize + 6)
+            .text(d => d.data.name);
+
+        node.append("title")
+            .text(d => d.ancestors().reverse().map(d => d.data.name).join("/"));
+
+        for (const {label, value, format, x} of columns) {
+            svg.append("text")
+                .attr("dy", "0.32em")
+                .attr("y", -nodeSize)
+                .attr("x", x)
+                .attr("text-anchor", "end")
+                .attr("font-weight", "bold")
+                .text(label);
+
+            node.append("text")
+                .attr("dy", "0.32em")
+                .attr("x", x)
+                .attr("text-anchor", "end")
+                .attr("fill", d => d.children ? null : "#555")
+            .data(root.copy().sum(value).descendants())
+                .text(d => format(d.value, d));
+        }
+
+        return svg.node();
+    }
+
+    async plot(dataSetUrl, divNodeId) {
+        const data = await this.fetchData(dataSetUrl);
+        const chart = await this.computeChart(data);
+        document.querySelector(divNodeId).append(chart);
+    }
+}
+
+class OctoAxxelAccelerationVis {
 
     /**
      * @param {str} fileUrl - URL of tabular separated file, i.e. "plugin/octoprint_accelerometer/download/axxel-30f9c95c-20231127-235625233-s000-ax-f010-z015.tsv"
      * @param {char} separator - tabular separator (single character)
      * @return {[{seq: float, sample: float, x: float, y: float, z: float}]}
      */
-    async fetchAccelerationData(fileUrl, separator = " ") {
+    async fetchData(fileUrl, separator = " ") {
         return d3.dsv(separator, fileUrl, (line) => {
                 return {
                     seq:    parseFloat(line.seq),
@@ -23,7 +141,7 @@ class OctoAxxelDataVis {
     /**
      * @param {[{seq: float, sample: float, x: float, y: float, z: float}]}: data - chart data to plot
      */
-    async computeAccelerationChart(data) {
+    async computeChart(data) {
         // declare the chart dimensions and margins
         const width = 640;
         const height = 400;
@@ -89,12 +207,12 @@ class OctoAxxelDataVis {
             .attr("stroke-width", 1.5)
             .attr("d", line(data));
 
-        return self.svg = svg.node();
+        return svg.node();
     }
 
     async plot(fileUrl, divNodeId) {
-        const data = await this.fetchAccelerationData(fileUrl);
-        const chart = await this.computeAccelerationChart(data);
+        const data = await this.fetchData(fileUrl);
+        const chart = await this.computeChart(data);
         document.querySelector(divNodeId).append(chart);
     }
 }
